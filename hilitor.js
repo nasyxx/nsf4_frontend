69b81c610004f05f459a8187e74d8d9bd172278d
Nasy <nasyxx+git@gmail.com>
2020-01-10 14:08:33 -0500

    Only one color.

--- hilitor.js
+++ hilitor.js
@@ -9,7 +9,7 @@ function Hilitor(id, tag)
     var targetNode = document.getElementById(id) || document.body;
     var hiliteTag = tag || "MARK";
     var skipTags = new RegExp("^(?:" + hiliteTag + "|SCRIPT|FORM|SPAN)$");
-    var colors = ["#ff6", "#a0ffff", "#9f9", "#f99", "#f6f"];
+    var colors = ["#ff6"];
     var wordColor = [];
     var colorIdx = 0;
     var matchRegExp = "";
