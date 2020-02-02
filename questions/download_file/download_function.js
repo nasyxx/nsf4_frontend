69c6e610e083e69a54cafc4ca56abeb4f3f6bfc9
derek <wl328@njit.edu>
2020-02-02 14:53:38 -0500

    change the download file

--- questions/download_file/download_function.js
+++ questions/download_file/download_function.js
@@ -2,18 +2,21 @@ function download_file() {
     const link  =document.createElement('a')
     const body = document.querySelector('body')
     if (document.getElementById('download').value === 'bert'){
-        link.href = 'questions/download_file/bert.json'
-        link.download = 'bert.json'
+        link.href = 'questions/download_file/bert.csv'
+        link.download = 'bert.csv'
         link.style.display = 'none'
     }else if (document.getElementById('download').value === 'bow'){
-        link.href = 'questions/download_file/bow.json'
-        link.download = 'bow.json'
+        link.href = 'questions/download_file/bow.csv'
+        link.download = 'bow.csv'
+        link.style.display = 'none'
     }else if (document.getElementById('download').value === 'glove'){
-        link.href = 'questions/download_file/glove.json'
-        link.download = 'glove.json'
+        link.href = 'questions/download_file/glove.csv'
+        link.download = 'glove.csv'
+        link.style.display = 'none'
     }else if (document.getElementById('download').value === 'tfidf'){
-        link.href = 'questions/download_file/tfidf.json'
-        link.download = 'tfidf.json'
+        link.href = 'questions/download_file/tfidf.csv'
+        link.download = 'tfidf.csv'
+        link.style.display = 'none'
     }
     body.appendChild(link)
     link.click()
