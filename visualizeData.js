14995fad73be46f6ee52280ba75064d98bf04201
derek <wl328@njit.edu>
2020-02-09 15:52:32 -0500

    add the legend

--- visualizeData.js
+++ visualizeData.js
@@ -1,12 +1,34 @@
-let domain_question
+let bio_data
+let wq_data
+let fire_data
+let gis_data
 function read_data(question_data) {
-    let dd = []
+    let b_wq = []
+    let b_fire = []
+    let b_bio = []
+    let b_gis = []
     let ll = question_data.length
 
     for (let i = 0; i < ll; i++) {
         let d = [question_data[i].x, question_data[i].y, question_data[i].question, question_data[i].catagory,question_data[i].source];
-        dd.push(d);
-        domain_question= dd
+        switch (d[4]){
+            case 'wq':
+                b_wq.push(d)
+                break
+            case 'fire':
+                b_fire.push(d)
+                break
+            case 'bio':
+                b_bio.push(d)
+                break
+            case 'gis':
+                b_gis.push(d)
+                break
+        }
+        bio_data = b_bio
+        wq_data = b_wq
+        fire_data = b_fire
+        gis_data = b_gis
     }
 
 }
@@ -37,7 +59,7 @@ function load_visualize (){
             formatter: function (params) {
 
                 return '<div class="tool_div">' + params.seriesName + ' :<br/>' + params.value[2] + '<br/>'
-                    + 'category number: ' + params.value[3] + '<br/>' + 'source: ' + params.value[4] + '</div>'
+                    + 'category number: ' + params.value[3] + '<br/>' + '</div>'
 
             },
 
@@ -75,6 +97,13 @@ function load_visualize (){
             },
 
         ],
+        color:['#F75000','#F75000','#F75000','#F75000'],
+        legend: {
+            data: ['Water Quality', 'Wild File','BIO','GIS'],
+            top: "10%",
+            right: "6%",
+
+        },
 
         xAxis: [
             {
@@ -102,29 +131,10 @@ function load_visualize (){
         ],
         series: [
             {
-                name: 'question',
+                name: 'Water Quality',
                 type: 'scatter',
-                data: domain_question,
-                symbol: function (domain_question, params) {
-                    let symbol
-                    switch (params.value[4]) {
-                        case 'wq':
-                            symbol = 'rect'
-                            break
-                        case 'fire':
-                            symbol = 'circle'
-                            break
-                        case 'bio':
-                            symbol = 'arrow'
-                            break
-                        case 'gis':
-                            symbol = 'triangle'
-                            break
-
-                    }
-                    return symbol
-
-                },
+                data: wq_data,
+                symbol: 'rect',
                 itemStyle: {
                     color: function (params) {
                         let color
@@ -145,10 +155,10 @@ function load_visualize (){
                                 color = "grey"
                                 break
                             case 5:
-                                color = "red"
+                                color = "#AE0000"
                                 break
                             case 6:
-                                color = "Aquamarine"
+                                color = '#336666'
                                 break
                             case 7:
                                 color = "purple"
@@ -163,31 +173,135 @@ function load_visualize (){
                         return color
                     }
                 },
-
-                markArea: {
-                    silent: true,
-                    itemStyle: {
-                        color: 'transparent',
-                        borderWidth: 1,
-                        borderType: 'dashed'
-                    },
-                    data: [[{
-                        name: '',
-                        xAxis: 'min',
-                        yAxis: 'min'
-                    }, {
-                        xAxis: 'max',
-                        yAxis: 'max'
-                    }]]
+            },{
+                name: 'Wild File',
+                type: 'scatter',
+                data: fire_data,
+                symbol: 'circle',
+                itemStyle: {
+                    color: function (params) {
+                        let color
+                        switch (params.value[3]) {
+                            case 0:
+                                color = "black"
+                                break
+                            case 1:
+                                color = "green"
+                                break
+                            case 2:
+                                color = "blue"
+                                break
+                            case 3:
+                                color = "orange"
+                                break
+                            case 4:
+                                color = "grey"
+                                break
+                            case 5:
+                                color = "#AE0000"
+                                break
+                            case 6:
+                                color = '#336666'
+                                break
+                            case 7:
+                                color = "purple"
+                                break
+                            case 8:
+                                color = "fuchsia"
+                                break
+                            case 9:
+                                color = "maroon"
+                                break
+                        }
+                        return color
+                    }
                 },
-
-                markPoint: {
-                    data: [
-                        {type: 'max', name: 'maxValue'},
-                        {type: 'min', name: 'miniValue'}
-                    ]
+            },{
+                name: 'BIO',
+                type: 'scatter',
+                data: bio_data,
+                symbol: 'diamond',
+                itemStyle: {
+                    color: function (params) {
+                        let color
+                        switch (params.value[3]) {
+                            case 0:
+                                color = "black"
+                                break
+                            case 1:
+                                color = "green"
+                                break
+                            case 2:
+                                color = "blue"
+                                break
+                            case 3:
+                                color = "orange"
+                                break
+                            case 4:
+                                color = "grey"
+                                break
+                            case 5:
+                                color = "#AE0000"
+                                break
+                            case 6:
+                                color = '#336666'
+                                break
+                            case 7:
+                                color = "purple"
+                                break
+                            case 8:
+                                color = "fuchsia"
+                                break
+                            case 9:
+                                color = "maroon"
+                                break
+                        }
+                        return color
+                    }
                 },
-
+            },{
+                name: 'GIS',
+                type: 'scatter',
+                data: gis_data,
+                symbol: 'triangle',
+                itemStyle: {
+                    color: function (params) {
+                        let color
+                        switch (params.value[3]) {
+                            case 0:
+                                color = "black"
+                                break
+                            case 1:
+                                color = "green"
+                                break
+                            case 2:
+                                color = "blue"
+                                break
+                            case 3:
+                                color = "orange"
+                                break
+                            case 4:
+                                color = "grey"
+                                break
+                            case 5:
+                                color = "#AE0000"
+                                break
+                            case 6:
+                                color = '#336666'
+                                break
+                            case 7:
+                                color = "purple"
+                                break
+                            case 8:
+                                color = "fuchsia"
+                                break
+                            case 9:
+                                color = "maroon"
+                                break
+                        }
+                        return color
+                    }
+                },
             },
 
         ]
